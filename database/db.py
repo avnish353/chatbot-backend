dad1ed9a8a939b32ab4dc6fcbd20a6147d24e63b
Avnish Sharma <avnishsharma6268@gmail.com>
2026-07-08 11:54:22 +0530

    Update database connection in db.py
    Refactor database connection parameters to use new environment variable names.

--- database/db.py
+++ database/db.py
@@ -1,15 +1,13 @@
 import os
+import mysql.connector
 from dotenv import load_dotenv
-import mysql.connector # type: ignore
 
 load_dotenv()
 
-def get_connection():
-    return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT")),
-        ssl_disabled=False
-    )
+conn = mysql.connector.connect(
+    host=os.getenv("MYSQLHOST"),
+    port=int(os.getenv("MYSQLPORT")),
+    user=os.getenv("MYSQLUSER"),
+    password=os.getenv("MYSQLPASSWORD"),
+    database=os.getenv("MYSQLDATABASE")
+)
